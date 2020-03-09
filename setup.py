5bd85260086f860a4ba4bfe5eefc47e1cff65e3f
Peter Law <PeterJCLaw@gmail.com>
2020-03-09 21:56:13 +0000

    Drop support for older versions of Python

--- setup.py
+++ setup.py
@@ -1,26 +1,9 @@
-import sys
-
 from setuptools import find_packages, setup
 
 
 with open('README.rst') as f:
     long_description = f.read()
 
-install_requires = [
-    'python-dateutil >=2.2, <3',
-    'paramiko >=1.10, <3',
-    'sr.comp >=1.0, <2',
-    'reportlab >=3.1.44, <3.5',
-    'requests >=2.5.1, <3',
-    'ruamel.yaml >=0.13.0, <0.16',
-    'simplejson >=3.6, <4',
-    'six >=1.9, <2',
-    'Pillow >=2.7, <7',
-    'mido >=1.1, <2'
-]
-
-if sys.version_info < (3, 4):
-    install_requires.append('pathlib >=1.0, <2')
 
 setup(
     name='sr.comp.cli',
@@ -31,7 +14,19 @@ setup(
     long_description=long_description,
     author='Student Robotics Competition Software SIG',
     author_email='srobo-devel@googlegroups.com',
-    install_requires=install_requires,
+    install_requires=[
+        'python-dateutil >=2.2, <3',
+        'paramiko >=1.10, <3',
+        'sr.comp >=1.0, <2',
+        'reportlab >=3.1.44, <3.5',
+        'requests >=2.5.1, <3',
+        'ruamel.yaml >=0.13.0, <0.16',
+        'simplejson >=3.6, <4',
+        'six >=1.9, <2',
+        'Pillow >=2.7, <7',
+        'mido >=1.1, <2',
+    ],
+    python_requires='>=3.5',
     entry_points={
         'console_scripts': [
             'srcomp = sr.comp.cli.command_line:main'
@@ -44,6 +39,14 @@ setup(
         'mock >=1.0.1',
         'nose >=1.3, <2',
         'freezegun >=0.3, <0.4',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
     ],
     test_suite='nose.collector'
 )
