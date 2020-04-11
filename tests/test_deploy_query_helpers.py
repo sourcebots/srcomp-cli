416e1249eafa8b091cd158f131549c058e94f508
Peter Law <PeterJCLaw@gmail.com>
2020-04-11 17:43:48 +0100

    Switch to a slightly more usual mock usage

--- tests/test_deploy_query_helpers.py
+++ tests/test_deploy_query_helpers.py
@@ -3,84 +3,80 @@ from unittest import mock
 
 from sr.comp.cli.deploy import query, query_bool
 
-input_name = 'sr.comp.cli.deploy.get_input'
+
+def mock_get_input(autospec=True, **kwargs):
+    return mock.patch('sr.comp.cli.deploy.get_input', autospec=autospec, **kwargs)
 
 
 class DeployQueryHelpersTests(unittest.TestCase):
     # Test `query`
 
-    @mock.patch(input_name, lambda x: 'nope')
-    def test_query_with_default_nope(self):
+    @mock_get_input(return_value='nope')
+    def test_query_with_default_nope(self, _):
         res = query('Question?', ('a'), default='a')
         self.assertEqual('a', res)
 
-    @mock.patch(input_name, lambda x: 'a')
-    def test_query_with_default_a(self):
+    @mock_get_input(return_value='a')
+    def test_query_with_default_a(self, _):
         res = query('Question?', ('a', 'b'), default='b')
         self.assertEqual('a', res)
 
-    @mock.patch(input_name)
-    def test_query_nope_then_a(self, mock_get_input):
-        values = ['nope', 'a']
-        mock_get_input.side_effect = lambda x: values.pop(0)
-
+    @mock_get_input(side_effect=iter(['nope', 'a']))
+    def test_query_nope_then_a(self, mocked_get_input):
         res = query('Question?', ('a', 'b'))
         self.assertEqual('a', res)
 
-        self.assertEqual(2, mock_get_input.call_count)
+        self.assertEqual(2, mocked_get_input.call_count)
 
     # Test `query_bool` true results
 
-    @mock.patch(input_name, lambda x: 'y')
-    def test_query_bool_True_y(self):
+    @mock_get_input(return_value='y')
+    def test_query_bool_True_y(self, _):
         res = query_bool('Question?', True)
         self.assertTrue(res)
 
-    @mock.patch(input_name, lambda x: 'n')
-    def test_query_bool_True_n(self):
+    @mock_get_input(return_value='n')
+    def test_query_bool_True_n(self, _):
         res = query_bool('Question?', True)
         self.assertFalse(res)
 
-    @mock.patch(input_name, lambda x: 'other')
-    def test_query_bool_True_other(self):
+    @mock_get_input(return_value='other')
+    def test_query_bool_True_other(self, _):
         res = query_bool('Question?', True)
         self.assertTrue(res)
 
     # Test `query_bool` false results
 
-    @mock.patch(input_name, lambda x: 'y')
-    def test_query_bool_False_y(self):
+    @mock_get_input(return_value='y')
+    def test_query_bool_False_y(self, _):
         res = query_bool('Question?', False)
         self.assertTrue(res)
 
-    @mock.patch(input_name, lambda x: 'n')
-    def test_query_bool_False_n(self):
+    @mock_get_input(return_value='n')
+    def test_query_bool_False_n(self, _):
         res = query_bool('Question?', False)
         self.assertFalse(res)
 
-    @mock.patch(input_name, lambda x: 'other')
-    def test_query_bool_False_other(self):
+    @mock_get_input(return_value='other')
+    def test_query_bool_False_other(self, _):
         res = query_bool('Question?', False)
         self.assertFalse(res)
 
     # Test `query_bool` without default
 
-    @mock.patch(input_name, lambda x: 'y')
-    def test_query_bool_no_default_y(self):
+    @mock_get_input(return_value='y')
+    def test_query_bool_no_default_y(self, _):
         res = query_bool('Question?')
         self.assertTrue(res)
 
-    @mock.patch(input_name, lambda x: 'n')
-    def test_query_bool_no_default_n(self):
+    @mock_get_input(return_value='n')
+    def test_query_bool_no_default_n(self, _):
         res = query_bool('Question?')
         self.assertFalse(res)
 
-    @mock.patch(input_name)
-    def test_query_bool_no_default_other_then_y(self, mock_get_input):
-        values = ['other', 'y']
-        mock_get_input.side_effect = lambda x: values.pop(0)
-
+    @mock_get_input(side_effect=iter(['other', 'y']))
+    def test_query_bool_no_default_other_then_y(self, mocked_get_input):
         res = query_bool('Question?')
         self.assertTrue(res)
 
-        self.assertEqual(2, mock_get_input.call_count)
+        self.assertEqual(2, mocked_get_input.call_count)
