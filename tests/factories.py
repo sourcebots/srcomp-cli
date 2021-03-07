3c103a418cba19e6281c01fc84e933108cfb0a27
Peter Law <PeterJCLaw@gmail.com>
2021-03-07 00:28:42 +0000

    Avoid calls in argument defaults

--- tests/factories.py
+++ tests/factories.py
@@ -6,13 +6,16 @@ from dateutil.tz import UTC
 from sr.comp.match_period import Match, MatchType
 from sr.comp.types import ArenaName, MatchNumber, TLA
 
+_DEFAULT_START_TIME = datetime.datetime(2020, 1, 25, 11, 0, tzinfo=UTC)
+_DEFAULT_END_TIME = datetime.datetime(2020, 1, 25, 11, 5, tzinfo=UTC)
+
 
 def build_match(
     num: int = 0,
     arena: str = 'main',
     teams: Sequence[Optional[TLA]] = (),
-    start_time: datetime.datetime = datetime.datetime(2020, 1, 25, 11, 0, tzinfo=UTC),
-    end_time: datetime.datetime = datetime.datetime(2020, 1, 25, 11, 5, tzinfo=UTC),
+    start_time: datetime.datetime = _DEFAULT_START_TIME,
+    end_time: datetime.datetime = _DEFAULT_END_TIME,
     type_: MatchType = MatchType.league,
     use_resolved_ranking: bool = False,
 ) -> Match:
