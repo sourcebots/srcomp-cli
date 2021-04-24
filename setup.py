5d7400c263320da68c3c493ce997265012156233
Peter Law <PeterJCLaw@gmail.com>
2021-04-24 11:56:22 +0100

    Declare 1.4.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('setup-requirements.txt') as f:
 
 setup(
     name='sr.comp.cli',
-    version='1.3.1',
+    version='1.4.0',
     project_urls={
         'Documentation': 'https://srcomp-cli.readthedocs.org/',
         'Code': 'https://github.com/PeterJCLaw/srcomp-cli',
