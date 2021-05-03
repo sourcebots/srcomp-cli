45b9c97d25c8523961b0185dfcafa9761bce238b
WillB97 <will@rudi.org.uk>
2021-05-03 18:00:33 +0100

    Add formatting to tied positions

--- sr/comp/cli/show_league_table.py
+++ sr/comp/cli/show_league_table.py
@@ -9,6 +9,7 @@ SORT_TYPES = {  # cli string: (sort column, sort direction)
 
 def command(settings):
     import os.path
+    from collections import Counter
 
     from sr.comp.comp import SRComp
     from tabulate import tabulate
@@ -19,17 +20,26 @@ def command(settings):
     for team in comp.teams.values():
         scores = comp.scores.league.teams[team.tla]
         league_pos = comp.scores.league.positions[team.tla]
-        league_table.append((
+        league_table.append([
             league_pos,
             scores.league_points,
             scores.game_points,
             team.tla,
             team.name,
-        ))
+        ])
 
     # sort rows
     sort_col, sort_direction = SORT_TYPES[settings.sort]
     league_table.sort(key=lambda row: row[sort_col], reverse=sort_direction)
+
+    # calculate ties
+    tie_count = Counter(rank for rank, _, _, _, _ in league_table)
+    ties = [rank for rank, count in tie_count.items() if count > 1]
+
+    # apply tie formatting
+    for team_data in league_table:
+        if team_data[0] in ties:
+            team_data[0] = f"={team_data[0]}"
 
     # Print table
     print(tabulate(
