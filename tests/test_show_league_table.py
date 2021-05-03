ac5ac873c66f162ae2577a98b0e3ddd76266bde0
WillB97 <will@rudi.org.uk>
2021-05-03 19:58:46 +0100

    Add basic test

---
+++ tests/test_show_league_table.py
@@ -0,0 +1,17 @@
+import os.path
+import subprocess
+import unittest
+
+
+class LeagueTableTests(unittest.TestCase):
+    def test_league_table_succeeds(self):
+        test_dir = os.path.dirname(os.path.abspath(__file__))
+        dummy_compstate = os.path.join(test_dir, 'dummy')
+
+        result = subprocess.run(
+            ['srcomp', 'show-league-table', dummy_compstate],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+
+        self.assertEqual(0, result.returncode, result.stdout)
