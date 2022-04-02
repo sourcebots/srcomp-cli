sourcebots/srcomp-cli

commit 780cad684f5837febfff4056e1de990a0e51de33

Canonicalise around using ruamel.yaml in the CLI

sr/comp/cli/add_delay.py
@@ -108,10 +108,10 @@ def add_delay(schedule, delay_seconds, when):
 def command(settings):
     import os.path
 
-    from sr.comp.cli import yaml_round_trip as rtyaml
+    from sr.comp.cli import yaml_round_trip as yaml
 
     schedule_path = os.path.join(settings.compstate, "schedule.yaml")
-    schedule = rtyaml.load(schedule_path)
+    schedule = yaml.load(schedule_path)
 
     how_long = parse_duration(settings.how_long)
     how_long_seconds = how_long.seconds
@@ -121,7 +121,7 @@ def command(settings):
 
     add_delay(schedule, how_long_seconds, when)
 
-    rtyaml.dump(schedule_path, schedule)
+    yaml.dump(schedule_path, schedule)
 
     return how_long, when
 

sr/comp/cli/schedule_league.py
@@ -19,8 +19,7 @@ def command(args):
     import sys
     from multiprocessing import Pool
 
-    import yaml
-
+    from sr.comp.cli import yaml_round_trip as yaml
     from sr.comp.cli.league_scheduler import Scheduler
 
     with open(os.path.join(args.compstate, 'arenas.yaml')) as f:

sr/comp/cli/update_layout.py
@@ -27,10 +27,10 @@ class Takeable:
 def command(settings):
     import os.path
 
-    from sr.comp.cli import yaml_round_trip as rtyaml
+    from sr.comp.cli import yaml_round_trip as yaml
 
     layout_yaml = os.path.join(settings.compstate, 'layout.yaml')
-    layout = rtyaml.load(layout_yaml)
+    layout = yaml.load(layout_yaml)
     layout_teams = layout['teams']
 
     with open(settings.teams_list) as tlf:
@@ -51,7 +51,7 @@ def command(settings):
     if teams.has_more:
         layout_teams[-1]['teams'] += teams.remainder
 
-    rtyaml.dump(layout_yaml, layout)
+    yaml.dump(layout_yaml, layout)
 
     print("Layout updated. You should consider re-importing the schedule now.")
 

sr/comp/cli/import_schedule/loading.py
@@ -34,7 +34,7 @@ def dump_league_yaml(
     matches: Dict[MatchNumber, RawMatch],
     file_path: Path,
 ) -> None:
-    import yaml
+    from sr.comp.cli import yaml_round_trip as yaml
 
     with open(file_path, 'w') as lfp:
         empty = {'matches': matches}
@@ -42,7 +42,7 @@ def dump_league_yaml(
 
 
 def load_league_yaml(league_yaml: Path) -> Dict[MatchNumber, RawMatch]:
-    import yaml
+    from sr.comp.cli import yaml_round_trip as yaml
 
     with open(league_yaml) as lfp:
         data = yaml.load(lfp)

sr/comp/cli/import_schedule/teams_mapping.py
@@ -35,8 +35,7 @@ def order_teams_by_location(layout_yaml: Path, team_ids: List[TLA]) -> List[TLA]
     This is useful as it should mean that the scrutineers can move around the
     venue easily visiting teams in the order which they first appear in matches.
     """
-    import yaml
-
+    from sr.comp.cli import yaml_round_trip as yaml
     from sr.comp.validation import join_and
 
     with open(layout_yaml) as lf:

commit 4afc16738212a0eb7fc041a0eee7174248969e32

Make our round-trip YAML dump look more like other similar functions
Specifically `json.dump` and PyYAML's `yaml.dump` both take their
data first and file second.

This goes towards fixing compatibility that was broken in
780cad684f5837febfff4056e1de990a0e51de33.

sr/comp/cli/add_delay.py
@@ -125,7 +125,7 @@ def command(settings: argparse.Namespace) -> Tuple[datetime.timedelta, datetime.
 
     add_delay(schedule, how_long_seconds, when)
 
-    yaml.dump(schedule_path, schedule)
+    yaml.dump(schedule, dest=schedule_path)
 
     return how_long, when
 

sr/comp/cli/schedule_league.py
@@ -62,7 +62,7 @@ def command(args: argparse.Namespace) -> None:
         pool = Pool(args.parallel)
 
         def get_output(data):
-            yaml.dump({'matches': data}, sys.stdout)
+            yaml.dump({'matches': data}, dest=sys.stdout)
             pool.terminate()
 
         for n in range(args.parallel):
@@ -74,7 +74,7 @@ def command(args: argparse.Namespace) -> None:
         pool.join()
     else:
         output_data = scheduler.run()
-        yaml.dump({'matches': output_data}, sys.stdout)
+        yaml.dump({'matches': output_data}, dest=sys.stdout)
 
 
 def add_subparser(subparsers: argparse._SubParsersAction[argparse.ArgumentParser]) -> None:

sr/comp/cli/update_layout.py
@@ -57,7 +57,7 @@ def command(settings: argparse.Namespace) -> None:
     if teams.has_more:
         layout_teams[-1]['teams'] += teams.remainder
 
-    yaml.dump(layout_yaml, layout)
+    yaml.dump(layout, dest=layout_yaml)
 
     print("Layout updated. You should consider re-importing the schedule now.")
 

sr/comp/cli/yaml_round_trip.py
@@ -21,7 +21,7 @@ def load(yaml_file):
         return ryaml.load(stream=yf)
 
 
-def dump(yaml_file, data):
+def dump(data, dest):
     import io
     ryaml = _load()
 
@@ -34,13 +34,13 @@ def dump(yaml_file, data):
             yaml = yaml[len(YAML_1_1_prefix):]
 
     yaml = "\n".join(x.rstrip() for x in yaml.splitlines())
-    with open(yaml_file, 'w') as yf:
+    with open(dest, 'w') as yf:
         print(yaml, file=yf)
 
 
 def command(settings):
     fp = settings.file_path
-    dump(fp, load(fp))
+    dump(load(fp), dest=fp)
 
 
 def add_subparser(subparsers):