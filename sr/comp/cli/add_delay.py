0f2f541f4fd15d6c56b774d6d4b33bd96d431c6b
Peter Law <PeterJCLaw@gmail.com>
2022-04-02 18:50:51 +0100

    Update help text to match behaviour
    This appears to have been missed when timelib was removed.

--- sr/comp/cli/add_delay.py
+++ sr/comp/cli/add_delay.py
@@ -143,9 +143,11 @@ def add_arguments(parser: argparse.ArgumentParser) -> None:
         nargs='?',
         default='now',
         help=(
-            "When the delay should occur. This can be anything which PHP's "
-            "strtotime would be able to parse. Assumes all times are in the "
-            "current timezone, regardless of input."
+            "When the delay should occur. This can be specified in a number of "
+            "formats: an absolute time (parsed by dateutil), 'now', "
+            "<duration> ago or in <duration> where <duration> is specified as a "
+            "number of seconds or a string of the form 1m30s. "
+            "Assumes all times are in the current timezone, regardless of input."
         ),
     )
 
