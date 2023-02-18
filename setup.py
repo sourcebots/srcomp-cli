b6bef9ebcee65705856e21af021ad9b16544d11c
Peter Law <PeterJCLaw@gmail.com>
2023-02-18 21:23:27 +0000

    Declare v1.6.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='sr.comp.cli',
-    version='1.6.0',
+    version='1.6.1',
     project_urls={
         'Documentation': 'https://srcomp-cli.readthedocs.org/',
         'Code': 'https://github.com/PeterJCLaw/srcomp-cli',
